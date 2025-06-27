841865112a312a289290d59669906b3d0ddecbc5
NestcodeShield <chaoshorizon2000@gmail.com>
2025-06-27 13:25:17 +0300

    Initial commit

---
+++ frontend/src/components/PostItem.jsx
@@ -0,0 +1,28 @@
+import { useState,useEffect } from "react";
+import React from "react";
+
+
+function PostItem() {
+
+  const post = [
+    {id:2313, url:'/images/portfolio-1.jpg', description:'Здесь должно быть описание',title:'Название поста',createdAt:'2023-05-01' },
+  ]
+
+  return (
+    
+    <div className="PostItem">
+     <div className="list-grid">
+          {post.map((post) => (
+            <div className="post-list-item">
+              <img src={post.url} alt={post.index} />
+              <span>{post.title}</span>
+              <p>{post.description}</p>
+              <small>{post.createdAt}</small>
+            </div>
+          ))}
+        </div>
+    </div>
+  );
+}
+
+export default PostItem;
